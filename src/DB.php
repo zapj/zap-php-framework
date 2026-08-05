77fdc67846a3a0723d6a076022a29acb48fbe718
z <z@zap.cn>
2026-08-05 10:16:01 +0800

    fix

--- src/DB.php
+++ src/DB.php
@@ -237,11 +237,11 @@ class DB
     /**
      * 执行原始 SQL 语句（SELECT / 任意），返回受影响行数
      */
-    public static function query(string $query, array $params = [])
+    public static function query(string $query, array $params = []): int
     {
         $stm = self::connection()->prepare($query);
         $stm->execute($params);
-        return $stm;
+        return $stm->rowCount();
     }
 
     /**
